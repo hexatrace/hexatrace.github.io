7753da6221b020ecad4706c79c961b6bf348de7e
luffy <1uffyD9>
2021-05-08 00:28:10 +0530

    delploying site without blogs

---
+++ assets/js/common.js
@@ -0,0 +1,109 @@
+$(function() {
+  'use strict';
+
+  /* =======================
+  // Reveal Image
+  ======================= */
+  var ww = window.innerWidth,
+    wh = window.innerHeight;
+
+  $(window).ready(function () {
+    $('body').waitForImages({
+      finished: function () {
+        setTimeout(function () {
+          $('.preloader').addClass('hide');
+
+          setTimeout(function () {
+            reveals();
+          }, 100);
+        }, 500);
+      },
+      waitForAll: true
+    });
+  });
+
+  function reveals() {
+    $(window).on('scroll', function () {
+      $(".article-box, .article-first, .post-cover-img-frame, .post-image-box, .page-image-box, .post-body img, .page-body img, .recent-header").each(
+        function(i) {
+          var el_top = $(this).offset().top,
+            win_bottom = wh + $(window).scrollTop();
+
+          if (el_top < win_bottom) {
+            $(this)
+              .delay(i * 100)
+              .queue(function() {
+                $(this).addClass("reveal-in");
+              });
+          }
+        }
+      );
+    }).scroll();
+  }
+
+
+  /* =======================
+  // Responsive Videos
+  ======================= */
+  $(".post-content, .page-content").fitVids({
+    customSelector: ['iframe[src*="ted.com"]']
+  });
+  
+
+  /* =======================
+  // Instagram Feed
+  ======================= */
+  // userId and accessToken from Matthew Elsom (https://codepen.io/matthewelsom/pen/zrrrLN) for example, for which he thanks a lot!
+  var instagramFeed = new Instafeed({
+    get: 'user',
+    limit: 6,
+    resolution: 'standard_resolution',
+    userId: '8987997106',
+    accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',
+    template:
+      '<li class="instagram-item"><a href="{{link}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>'
+  });
+
+  if ($('#instafeed').length) {
+    instagramFeed.run();
+  }
+
+
+  /* =======================
+  // Scroll Top Button
+  ======================= */
+  $(".top").click(function () {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
+});
+
+  /* =======================
+  // Toggle button
+  ======================= */
+
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
+
